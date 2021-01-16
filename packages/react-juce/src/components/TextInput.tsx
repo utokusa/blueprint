4f55bf85f780ce55f492937c71613858a2bef7d7
utokusa <utokusa@gmail.com>
2021-01-16 18:12:53 +0900

    Rewrite TextInput using class

--- packages/react-juce/src/components/TextInput.tsx
+++ packages/react-juce/src/components/TextInput.tsx
@@ -1,37 +1,23 @@
-import React from 'react';
+import React, { Component, PropsWithChildren } from 'react';
+import { SyntheticMouseEvent } from '../lib/SyntheticEvents';
 
-export function TextInput(props: any) {
-    return React.createElement('TextInput', props, props.children);
+// export function TextInput(props: any) {
+//     return React.createElement('TextInput', props, props.children);
+// }
+
+export interface TextInputProps {
+  placeholder?: string
+  maxlength?: number
+  onChange?: (value: number) => void
 }
 
-TextInput.WordWrap = {
-    none: 0,
-    byWord: 1,
-    byChar: 2,
-};
+type TextInputState = {
+}
 
-TextInput.FontStyleFlags = {
-    plain: 0,
-    bold: 1,
-    italic: 2,
-    underlined: 4,
-};
-
-TextInput.JustificationFlags = {
-    left: 1,
-    right: 2,
-    horizontallyCentred: 4,
-    top: 8,
-    bottom: 16,
-    verticallyCentred: 32,
-    horizontallyJustified: 64,
-    centred: 36,
-    centredLeft: 33,
-    centredRight: 34,
-    centredTop: 12,
-    centredBottom: 20,
-    topLeft: 9,
-    topRight: 10,
-    bottomLeft: 17,
-    bottomRight: 18,
+export class TextInput extends Component<PropsWithChildren<TextInputProps | any>, TextInputState> {
+  render() {
+    return (
+      React.createElement('TextInput', this.props, this.props.children)
+    );
+  }
 };
